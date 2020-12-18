7f7a2730eaf358dc960591975bbf1c1175d733c3
minsmseo <minsmseo@gmail.com>
2020-12-18 20:22:39 +0900

    Add scripts: GameController, SaveDataController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+    private bool isLoading;
+    public bool IsLoading { get { return isLoading;  } }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            isLoading = true;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isLoading = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SaveDataController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveDataController : MonoBehaviour
+{
+    [SerializeField]
+    private UserData user;
+    
+    protected void CreateUserData()
+    {
+        string name = "new user";
+        //initailize userdata
+        user = new UserData();
+        user.userName = name;
+    }
+}
+[SerializeField]
+public struct UserData
+{
+    public string userName;
+    //
+}
